070a3d324a8ce2eac646163fa44987345625e468
FlavioSant <flaviohenrique_santos@hotmail.com>
2022-06-24 20:23:21 -0300

    :sparkles: Adicionado graphql-codegen

--- src/pages/Subscribe.tsx
+++ src/pages/Subscribe.tsx
@@ -1,23 +1,13 @@
-import { gql, useMutation } from "@apollo/client";
 import { FormEvent, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { useCreateSubscriberMutation } from "../graphql/generated";
 import { Logo } from "../components/Logo";
-
-const CREATE_SUBSCRIBER_MUTATION = gql`
-  mutation CreateSubscriber($name: String!, $email: String!) {
-    createSubscriber(data: { name: $name, email: $email }) {
-      id
-    }
-  }
-`;
 
 export const Subscribe = () => {
   const navigate = useNavigate();
   const [fields, setFields] = useState({ name: "", email: "" });
 
-  const [createSubscriber, { loading }] = useMutation(
-    CREATE_SUBSCRIBER_MUTATION
-  );
+  const [createSubscriber, { loading }] = useCreateSubscriberMutation();
 
   const handleSubscribe = async (event: FormEvent) => {
     event.preventDefault();
